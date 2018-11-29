3e6b97233dffd7aef9cba2c16f6c7af082075275
Abdulmalek Al-Gahmi <aalgahmi@cs.weber.edu>
2018-11-29 13:16:26 -0700

    After thu class

--- include/graph.h
+++ include/graph.h
@@ -3,16 +3,95 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include "linked_list.h"
 #include "linked_queue.h"
 
 template <typename T>
 class Graph{
 public:
-  //TODO
+  Graph(int sz): size(sz), adjLists(new LinkedList<int>[sz]()), vertices(new T[sz]()){}
+
+  bool empty() const {
+    return size == 0;
+  }
+
+  void print() const {
+    for(int i = 0; i < size; i++){
+      std::cout << std::setw(5) << i << ": " ;
+      adjLists[i].print();
+      std::cout << std::endl;
+    }
+  }
+
+  LinkedList<int>* getAdjLists() const{
+    return adjLists;
+  }
+
+  T* getVertices() const {
+    return vertices;
+  }
+
+  void depthFirstTraversal(){
+    bool* visited = new bool[size]();
+
+    for(int i = 0; i < size; i++){
+      if(!visited[i]){
+        depthFirstTraversal(i, visited);
+      }
+    }
+
+    delete[] visited;
+  }
+
+  void breadthFirstTraversal(){
+    LinkedQueue<int> queue;
+    bool* visited = new bool[size]();
+
+    for(int i = 0; i < size; i++){
+      if(!visited[i]){
+        queue.enqueue(i);
+        visited[i] = true;
+        std::cout << vertices[i] << "\t";
+
+        while(!queue.empty()){
+          int u = queue.peekFront();
+          queue.dequeue();
+          
+          for(LinkedListIterator<int> it = adjLists[u].begin(); it != adjLists[u].end(); ++it){
+            int w = *it;
+            if(!visited[w]){
+              queue.enqueue(w);
+              visited[w] = true;
+              std::cout << vertices[w] << "\t";
+            }
+          }
+        }
+      }
+    }
+
+    delete[] visited;
+  }
 
 private:
-  //TODO
+  int size;
+  LinkedList<int>* adjLists;
+  //std::vector<LinkedList<int>> graph;
+  T* vertices;
+  //std::vector<T> vertices;
+
+  void depthFirstTraversal(int v, bool* visited){
+    visited[v] = true;
+    std::cout << vertices[v] << "\t";
+
+    LinkedListIterator<int> it;
+    for(it = adjLists[v].begin(); it != adjLists[v].end(); ++it){
+      int u = *it;
+      if(!visited[u]){
+        depthFirstTraversal(u, visited);
+      }
+    }
+  }
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "graph.h"
 
 using namespace std;
 
-void createGraph(Graph<int>& g) {
-  //TODO
+void createGraph(Graph<int>* &g) {
+  ifstream file("graph.txt");
+  int size;
+  file >> size;
+  file.ignore();
+  g = new Graph<int>(size);
+
+  string line;
+  while(getline(file, line)){
+    stringstream ss;
+    ss << line;
+    int v, adj;
+    char colon;
+
+    ss >> v >> colon;
+    while( ss >> adj){
+      g->getVertices()[v] = v;
+      g->getAdjLists()[v].insertFirst(adj);
+    }
+
+  }
+  file.close();
 }
 
 int main(){
-  //TODO
+  Graph<int>* g;
+  createGraph(g);
+  g->print();
+  
+  delete g;
 
   return 0;
 }
